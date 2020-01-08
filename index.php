c0e8a24db538ed5d8bd9c2289fc22acc1082f8d0
sdbisTeam <balineanu@gmail.com>
2020-01-08 17:26:06 +0200

    #4: index.php http://pm.5psolutions.net/public/index.php?path_info=projects/training-claudia-balineanu/tasks/4 - in the index file I tried to keep the cart as an array of product ids in the Session but i didn't succeed to take in the index.php file only the products that aren't in the cart

--- index.php
+++ index.php
@@ -9,16 +9,42 @@ $stmt = $conn->prepare("SELECT * FROM products");
 $stmt->execute();
 
 $var = $stmt->fetchAll();
+
 ?>
 <html>
-<head></head>
+    <head></head>
 <body>
-<?php if (count($var) > 0): ?>
-    <?php foreach ($var as $row): ?>
-        <?= trans('Prod') ?> <?= $row['title'] ?> <br />
-    <?php endforeach; ?>
-<?php else: ?>
-    No data
-<?php endif; ?>
+    <table>
+        <?php if (count($var) > 0): ?>
+            <?php foreach ($var as $row): ?>
+            <tr>
+                <td>
+                    <?= '<img src="images/' . $row['image'] . '" width="100" height="100" alt="Image product">' ?>
+                </td>
+                <td>
+                    <?= $row['title'] ?> <br /><!-- </?= trans('Prod') ?>  -->
+                    <?= $row['description'] ?> <br />
+                    <?= $row['price'] ?> <br />
+                </td>
+                <td>
+                    <a href="index.php?cart=<?= $row['id'] ?>">Add</a>
+                </td>
+            </tr>
+            <?php endforeach; ?>
+        <?php else: ?>
+             <?= trans('No data') ?>
+        <?php endif; ?>
+    </table>
+    <a href="cart.php">Go To Cart</a>
 </body>
 </html>
+<?php
+if (! isset($_SESSION)) {
+    session_start();
+}
+if (empty($_SESSION['cart'])) {
+    $_SESSION['cart'] = array();
+}
+if (isset($_GET['cart'])) {// nup sa nu mai adauge acelasi id de mai multe ori
+    array_push($_SESSION['cart'], $_GET['cart']);
+}
