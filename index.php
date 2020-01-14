5f360116fc51771ee9377ba815692478b26df3ac
sdbisTeam <balineanu@gmail.com>
2020-01-14 11:58:22 +0200

    #4: index.php http://pm.5psolutions.net/public/index.php?path_info=projects/training-claudia-balineanu/tasks/4 - fixed: remove all testing code and comments;  re-format for missed/added indentation; rename variable form 'cart' to 'id'; the execution statement separated from if/else block; remove variable $ids; alt attribute translated. not fixed: the folder with images to be a part of git without the content.

--- index.php
+++ index.php
@@ -1,61 +1,54 @@
 <?php
 session_start();
-//session_destroy();
+
 // the file with the connection to the database
 require_once 'common.php';
 
-if (! isset($_SESSION['cart'])) {
+if (!isset($_SESSION['cart'])) {
     $_SESSION['cart'] = array();
 }
 
 // if is already in the session, don't insert again
-if (isset($_GET['cart']) && ! in_array($_GET['cart'], $_SESSION['cart'])) {
-    $_SESSION['cart'][] = $_GET['cart'];
+if (isset($_GET['id']) && !in_array($_GET['id'], $_SESSION['cart'])) {
+    $_SESSION['cart'][] = $_GET['id'];
 }
 
 if (!count($_SESSION['cart'])) {
     // prepare a query for selecting the products on the database
-    $stmt = $conn->prepare("SELECT * FROM products");
-    // execute the query
-    $stmt->execute();
+    $stmt = $conn->prepare("SELECT * FROM Products");
 } else {
-    $ids = $_SESSION['cart'];
-    $arr = array_fill(0, count($ids), '?');
+    $arr = array_fill(0, count($_SESSION['cart']), '?');
     $qMarks = implode(',', $arr);
     $sql = "SELECT * FROM Products WHERE id NOT IN($qMarks)";
-    //print_r($ids);
-    //print_r($sql); die();
     $stmt = $conn->prepare($sql);
-    $stmt->execute($ids);
 }
+$stmt->execute($_SESSION['cart']);
 $rows = $stmt->fetchAll();
-//session_unset();
-//session_destroy();
 ?>
 <html>
-    <head></head>
-    <body>
-        <table>
-            <?php if ((count($rows) > 0)): ?>
-                <?php foreach ($rows as $row): ?>
-                <tr>
-                    <td>
-                        <img src="images/<?= $row['image'] ?>" width="100" height="100" alt="Image product">
-                    </td>
-                    <td>
-                        <?= $row['title'] ?> <br />
-                        <?= $row['description'] ?> <br />
-                        <?= $row['price'] ?> <br />
-                    </td>
-                    <td>
-                        <a href="index.php?cart=<?= $row['id'] ?>"><?= trans('Add') ?></a>
-                    </td>
-                </tr>
-                <?php endforeach; ?>
-            <?php else: ?>
-                 <?= trans('No data') ?>
-            <?php endif; ?>
-        </table>
-        <a href="cart.php"><?= trans('Go To Cart') ?></a>
-    </body>
+<head></head>
+<body>
+<table>
+    <?php if ((count($rows) > 0)): ?>
+        <?php foreach ($rows as $row): ?>
+            <tr>
+                <td>
+                    <img src="images/<?= $row['image'] ?>" width="100" height="100" alt="<?= trans('Image product') ?>">
+                </td>
+                <td>
+                    <?= $row['title'] ?> <br/>
+                    <?= $row['description'] ?> <br/>
+                    <?= $row['price'] ?> <br/>
+                </td>
+                <td>
+                    <a href="index.php?id=<?= $row['id'] ?>"><?= trans('Add') ?></a>
+                </td>
+            </tr>
+        <?php endforeach; ?>
+    <?php else: ?>
+        <?= trans('No data') ?>
+    <?php endif; ?>
+</table>
+<a href="cart.php"><?= trans('Go To Cart') ?></a>
+</body>
 </html>
