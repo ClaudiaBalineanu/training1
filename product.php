207968b1473531678e6707a21d3dced9b795f2e9
sdbisTeam <balineanu@gmail.com>
2020-01-16 10:47:32 +0200

    #8: product.php http://pm.5psolutions.net/public/index.php?path_info=projects/training-claudia-balineanu/tasks/8 - when add a product and choose the image doesn't take immediately the name of the file in the text box, but after save, the name is displayed in the box

--- product.php
+++ product.php
@@ -1,9 +1,15 @@
 <?php
 require_once 'common.php';
 
-if (isset($_GET['edit'])) {
-    $sql = "SELECT * FROM Products WHERE id=" . $_GET['edit'];
+session_start();
 
+if (!isset($_SESSION['edit'])) {
+    $_SESSION['edit'] = array();
+}
+
+if (isset($_GET['id'])) {
+    $_SESSION['edit'][] = $_GET['id'];
+    $sql = "SELECT * FROM Products WHERE id=" . $_GET['id'];
     $stmt = $conn->prepare($sql);
     $stmt->execute();
     $rows = $stmt->fetchAll();
@@ -15,26 +21,18 @@ if (isset($_GET['edit'])) {
     }
 }
 
-if (! empty($_FILES)) {
-    $img = pathinfo($_FILES["browse"]["name"]);
-
-    $target_dir = "images/";
-    $target_file = $img['filename'];
-    $imageFileType = strtolower($img['extension']);
-
-    $image = $_POST['image'];
-    $image = $img['basename'];
-
-}
-
-$titleErr = $descriptionErr = $priceErr = "";
-$title = $description = $price = "";
+$titleErr = $descriptionErr = $priceErr = $imageErr = "";
+$title = $description = $price = $image = "";
 if (isset($_POST['submit'])) {
     if ($_SERVER['REQUEST_METHOD'] == "POST") {
         if (empty($_POST['title'])) {
             $titleErr = "Title is required";
         } else {
             $title = strip_tags($_POST['title']);
+            // check if title only contains letters and whitespace
+            if (!preg_match("/^[a-zA-Z ]*$/", $title)) {
+                $titleErr = "Only letters and white space allowed";
+            }
         }
 
         if (empty($_POST['description'])) {
@@ -47,79 +45,67 @@ if (isset($_POST['submit'])) {
             $priceErr = "Price is required";
         } else {
             $price = strip_tags($_POST['price']);
-        }
-
-        if (empty($_POST['image'])) {
-            $imageErr = "Image";
-        } else {
-            $image = $_POST['image'];
+            // check if price contains double values
+            if (!preg_match("/^[0-9]*\.[0-9]+$/", $price)) {
+                $priceErr = "Only numbers(double) value allowed";
+            }
         }
 
     }
 
 
-    $uniq = uniqid() . '.' . $imageFileType;
+    if(isset($_POST['image'])) {
 
-    if (move_uploaded_file($_FILES['browse']['tmp_name'], $target_dir . $uniq)) {
-        //$_POST['image'] =
-        //echo "The file ". basename( $_FILES["browse"]["name"]). " has been uploaded.";
-        if (isset($_GET['edit'])) {
-            $sql = "UPDATE Products SET title='" . $title . "', description='"
-                . $description . "', price="
-                . $price . ", image='"
-                . $uniq . "')";
+        $image = pathinfo($_FILES["browse"]["name"]);
+        $_POST['image'] = $image['basename'];
 
+        $target_dir = "images/";
+        $target_file = $image['filename'];
+        $imageFileType = strtolower($image['extension']);
 
-            print_r($sql); die();
+        $uniq = uniqid() . '.' . $imageFileType;
+
+        if (move_uploaded_file($_FILES['browse']['tmp_name'], $target_dir . $uniq)) {
+            if (isset($_SESSION['edit']) && !empty($_SESSION['edit'])) {
+                $sql = "UPDATE Products SET title='" . $title . "', description='"
+                    . $description . "', price='"
+                    . $price . "', image='"
+                    . $uniq . "' WHERE id=" . $_SESSION['edit'][0];
+            } else {
+                $sql = "INSERT INTO Products(title, description, price, image) VALUES('" . $title . "', '"
+                    . $description . "', '"
+                    . $price . "', '"
+                    . $uniq . "')";
+            }
             $stmt = $conn->prepare($sql);
-            //$val = $target_dir . uniqid() . '.' . $imageFileType;
             $stmt->execute();
-        } else {
-            $sql = "INSERT INTO Products(title, description, price, image) VALUES('" . $title . "', '"
-                . $description . "', "
-                . $price . ", '"
-                . $uniq . "')";
-            //print_r($sql); die();
-            $stmt = $conn->prepare($sql);
-            //$val = $target_dir . uniqid() . '.' . $imageFileType;
-            $stmt->execute();
+            session_unset();
         }
     }
-
-
-
 }
-
-
 ?>
 <html>
-<head></head>
-    <body>
-        <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" enctype="multipart/form-data">
-            <input type="text" name="title" placeholder="Title" value="<?php echo isset($_POST['title']) ? $_POST['title'] : '' ?>" />
-            <span class="error">* <?php echo $titleErr;?></span><br /><br />
-            <input type="text" name="description" placeholder="Description" value="<?php echo isset($_POST['description']) ? $_POST['description'] : '' ?>" />
-            <span class="error">* <?php echo $descriptionErr;?></span><br /><br />
-            <input type="text" name="price" placeholder="Price" value="<?php echo isset($_POST['price']) ? $_POST['price'] : '' ?>" />
-            <span class="error">* <?php echo $priceErr;?></span><br /><br />
-            <input type="text" name="image" placeholder="Image" value="<?php echo isset($_POST['image']) ? $_POST['image'] : '' ?>"  />
-
-            <input type="file" name="browse" id="browse" value="Browse" > <br><br>
-            <a href="products.php"><?= trans('Products') ?></a>
-            <input type="submit" name="submit" value="Save">
-        </form>
-
-
-        <!--  -->
-
-    </body>
+<head>
+    <style>
+        .error {color: #FF0000;}
+    </style>
+</head>
+<body>
+<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" enctype="multipart/form-data">
+    <input type="text" name="title" placeholder="Title"
+           value="<?php echo isset($_POST['title']) ? $_POST['title'] : '' ?>"/>*<br/>
+    <span class="error"><?php echo $titleErr; ?></span><br/>
+    <input type="text" name="description" placeholder="Description"
+           value="<?php echo isset($_POST['description']) ? $_POST['description'] : '' ?>"/>*<br/>
+    <span class="error"><?php echo $descriptionErr; ?></span><br/>
+    <input type="text" name="price" placeholder="Price"
+           value="<?php echo isset($_POST['price']) ? $_POST['price'] : '' ?>"/>*<br/>
+        <span class="error"><?php echo $priceErr; ?></span><br/>
+    <input type="text" name="image" placeholder="Image"
+           value="<?php echo isset($_POST['image']) ? $_POST['image'] : '' ?>"/>
+    <input type="file" name="browse" id="browse" value="Browse"><br/><br/>
+    <a href="products.php"><?= trans('Products') ?></a>
+    <input type="submit" name="submit" value="Save">
+</form>
+</body>
 </html>
-<?php
-/*
-print_r($title);
-print_r("*******");
-print_r($img);
-print_r("*******");
-print_r($_POST['image']);
-print_r("*******");
-*/
