6d90e5f0fde24df5404b54c4262644249fef938d
sdbisTeam <balineanu@gmail.com>
2020-01-21 18:33:13 +0200

    #5: cart.php http://pm.5psolutions.net/public/index.php?path_info=projects/training-claudia-balineanu/tasks/5 -

--- cart.php
+++ cart.php
@@ -1,44 +1,46 @@
 <?php
 require_once 'common.php';
 
-session_start();
-
 if (isset($_SESSION['cart'])) {
-    if (!count($_SESSION['cart'])) {
-        echo 'NO PRODUCTS IN CART';
-    } else {
+    if (count($_SESSION['cart'])) {
         if (isset($_GET['id'])) {
-            $key = array_search($_GET['id'], $_SESSION['cart']);
-            unset($_SESSION['cart'][$key]);
-            $_SESSION['cart'] = array_values($_SESSION['cart']);
-            header("Location: cart.php");
-            exit();
+            $keySession = array_search($_GET['id'], $_SESSION['cart']);
+            if (isset($keySession)) {
+                unset($_SESSION['cart'][$keySession]);
+                // remake the session array (in unset 2=>2, the 3=>3 will become 2=>3)
+                $_SESSION['cart'] = array_values($_SESSION['cart']);
+            }
+            redirect('cart.php');
         }
-        $arr = array_fill(0, count($_SESSION['cart']), '?');
-        $qMarks = implode(',', $arr);
-        $sql = "SELECT * FROM  Products WHERE id IN($qMarks)";
-        $stmt = $conn->prepare($sql);
-        $stmt->execute($_SESSION['cart']);
-        $rows = $stmt->fetchAll();
+        if (!empty($_SESSION['cart'])) {
+            $arr = array_fill(0, count($_SESSION['cart']), '?');
+            $qMarks = implode(',', $arr);
+            $stmt = $conn->prepare("SELECT * FROM  Products WHERE id IN($qMarks)");
+            $stmt->execute($_SESSION['cart']);
+            $rows = $stmt->fetchAll();
+        }
+    } else {
+        $mess = trans('NO PRODUCTS IN CART');
     }
 } else {
-    echo "Success checkout!";
+    $mess = trans('Successful checkout!');
 }
-$nameErr = $emailErr = "";
+
+$errors = array();
 $name = $email = $comment = "";
-$message = $mess = "";
+$message = "";
 
 if (isset($_POST['submit'])) {
 
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
         if (empty($_POST["name"])) {
-            $nameErr = "Name is required";
+            $errors['name'] = trans('Name is required');
         } else {
             $name = strip_tags($_POST["name"]);
         }
 
         if (empty($_POST["email"])) {
-            $emailErr = "Email is required";
+            $errors['email'] = trans('Email is required');
         } else {
             $email = strip_tags($_POST["email"]);
         }
@@ -50,100 +52,104 @@ if (isset($_POST['submit'])) {
         }
     }
 
-    $subject = strip_tags($_POST['name']);
+    $subject = trans("Email checkout");
     $from = strip_tags($_POST['email']);
-    $headers = "From:" . $from;
 
-    if (!empty($_SESSION['cart'])) {
+    $message = '<html><head></head><body><table>';
+    if (!empty($rows) && count($rows) > 0) {
         foreach ($rows as $row) {
-            $arr[] = $row['title'];
-            $message = implode(',', $arr);
+            $message .= '<tr><td><img src="images/' . $row['image'] .
+                '" width="100" height="100" alt="' . trans('Image product') . '"></td>';
+            $message .= '<td>' . $row['title'] . '<br/>' .
+                $row['description'] . '<br/>' .
+                $row['price'] . '<br/></td></tr>';
         }
+    } else {
+        echo $mess;
     }
-    /*
-    if ($subject and $from and $message) {
-        // need a mail server
-        $mail = mail(TO, $subject, $message, $headers);
+    $message .= '</table></body></html>';
+    // use wordwrap() if lines are longer than 70 characters
+    $message = wordwrap($message, 70);
+
+    // set content-type when sending HTML email
+    $headers = "MIME-Version: 1.0" . "\r\n";
+    $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+    $headers .= 'From: ' . $from . "\r\n";
+
+    if ($subject && $from && $message) {
+        // need a mail server and i don't have one so i just set a variable with a string !!!
+        // $mail = mail(TO, $subject, $message, $headers);
+        $mail = "yes";
     }
-    */
 
-    if ($subject and $from and $message) {
-        // need a mail server
-        //$mail = mail(TO, $subject, $message, $headers);
-        $mail = "da";
-    }
-
-    // to unset every time the checkout button is clicked
-    unset($_SESSION['checkout']);
-
-    // for cart checkout
     if (!isset($_SESSION['checkout'])) {
         $_SESSION['checkout'] = array();
     }
 
     $_SESSION['checkout'] = $_SESSION['cart'];
-
-
-    if (!$emailErr or !$nameErr) {
-        if (isset($_SESSION['checkout'])) {
-            $sqlQuery = "INSERT INTO Orders(email, name_cust) VALUES('" . $email . "', '" . $name . "')";
+    if (!isset($errors['name']) && !isset($errors['email'])) {
+        if (!empty($_SESSION['checkout'])) {
+            $sqlQuery = "INSERT INTO orders(email, name_cust) VALUES(?,?)";
             $smt = $conn->prepare($sqlQuery);
+            $smt->bindParam(1, $email, PDO::PARAM_STR, 50);
+            $smt->bindParam(2, $name, PDO::PARAM_STR, 50);
 
             if (count($smt->execute()) == 1) {
                 $lastId = $conn->lastInsertId();
                 foreach ($_SESSION['checkout'] as $id) {
-                    $query = "INSERT INTO order_product(order_id, product_id) VALUES('" . $lastId . "', '"
-                        . $id . "')";
+                    $query = "INSERT INTO order_product(order_id, product_id) VALUES(?,?)";
                     $stm = $conn->prepare($query);
+                    $stm->bindParam(1, $lastId, PDO::PARAM_INT);
+                    $stm->bindParam(2, $id, PDO::PARAM_INT);
                     $stm->execute();
                 }
             }
         }
     }
     if (isset($mail)) {
-        //unset($_SESSION['cart']);
-        session_unset();
+        unset($_SESSION['cart']);
         // or can be redirected to products page (index.php)
-        header("Location: cart.php");
-        exit();
+        redirect('cart.php');
     }
 }
 ?>
-    <html>
-    <head></head>
-    <body>
-    <table>
-        <?php if (!empty($rows) && count($rows) > 0): ?>
-            <?php foreach ($rows as $row): ?>
-                <tr>
-                    <td>
-                        <img src="images/<?= $row['image'] ?>" width="100" height="100"
-                             alt="<?= trans('Image product') ?>">
-                    </td>
-                    <td>
-                        <?= $row['title'] ?> <br/>
-                        <?= $row['description'] ?> <br/>
-                        <?= $row['price'] ?> <br/>
-                    </td>
-                    <td>
-                        <a href="cart.php?id=<?= $row['id'] ?>"><?= trans('Remove') ?></a>
-                    </td>
-                </tr>
-            <?php endforeach; ?>
-        <?php endif; ?>
-    </table>
-    <br/>
-    <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
-        <input type="text" name="name" placeholder="<?= trans('Name') ?>"
-               value="<?php echo isset($_POST['name']) ? $_POST['name'] : '' ?>"/>
-        <span class="error">* <?php echo $nameErr; ?></span><br/><br/>
-        <input type="email" name="email" placeholder="<?= trans('Email') ?>"
-               value="<?php echo isset($_POST['email']) ? $_POST['email'] : '' ?>"/>
-        <span class="error">* <?php echo $emailErr; ?></span><br/><br/>
-        <textarea name="comment" cols="20" rows="7" placeholder="<?= trans('Comment') ?>"></textarea><br/><br/>
-        <input type="submit" name="submit" value="<?= trans('Checkout') ?>">
-    </form>
-    <a href="index.php"><?= trans('Go to products') ?></a>
-    </body>
-    </html>
-<?php
+<html>
+<head></head>
+<body>
+<table>
+    <?php if (!empty($rows) && count($rows) > 0): ?>
+        <?php foreach ($rows as $row): ?>
+            <tr>
+                <td>
+                    <img src="images/<?= $row['image'] ?>" width="100" height="100"
+                         alt="<?= trans('Image product') ?>">
+                </td>
+                <td>
+                    <?= $row['title'] ?> <br/>
+                    <?= $row['description'] ?> <br/>
+                    <?= $row['price'] ?> <br/>
+                </td>
+                <td>
+                    <a href="cart.php?id=<?= $row['id'] ?>"><?= trans('Remove') ?></a>
+                </td>
+            </tr>
+        <?php endforeach; ?>
+    <?php else: ?>
+        <?= $mess; ?>
+    <?php endif; ?>
+</table>
+<br/>
+<form method="post">
+    <input type="text" name="name" placeholder="<?= trans('Name') ?>"
+           value="<?php echo isset($_POST['name']) ? $_POST['name'] : '' ?>"/>
+    <span class="error">* <?php echo isset($errors['name']) ? $errors['name'] : ''; ?></span><br/><br/>
+    <input type="email" name="email" placeholder="<?= trans('Email') ?>"
+           value="<?php echo isset($_POST['email']) ? $_POST['email'] : '' ?>"/>
+    <span class="error">* <?php echo isset($errors['email']) ? $errors['email'] : ''; ?></span><br/><br/>
+    <textarea name="comment" cols="20" rows="7"
+              placeholder="<?= trans('Comment') ?>"><?php echo isset($_POST['comment']) ? $_POST['comment'] : '' ?></textarea><br/><br/>
+    <input type="submit" name="submit" value="<?= trans('Checkout') ?>">
+</form>
+<a href="index.php"><?= trans('Go to products') ?></a>
+</body>
+</html>
