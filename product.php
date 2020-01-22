89e77efa020a597ebfa926abfbdfc12ebf4a3fcb
sdbisTeam <balineanu@gmail.com>
2020-01-22 18:16:09 +0200

    #8: product.php http://pm.5psolutions.net/public/index.php?path_info=projects/training-claudia-balineanu/tasks/8

--- product.php
+++ product.php
@@ -4,15 +4,14 @@ require_once 'common.php';
 setcookie('admin', session_id(), time() + (30 * 60));
 
 if (isset($_SESSION['admin']) && $_SESSION['admin'] == PASS) {
-
     if (!isset($_SESSION['edit'])) {
         $_SESSION['edit'] = array();
     }
 
     if (isset($_GET['id'])) {
         $_SESSION['edit'][] = $_GET['id'];
-        $sql = "SELECT * FROM Products WHERE id=" . $_GET['id'];
-        $stmt = $conn->prepare($sql);
+        $stmt = $conn->prepare("SELECT * FROM Products WHERE id=?");
+        $stmt->bindParam(1,$_GET['id'],PDO::PARAM_INT);
         $stmt->execute();
         $rows = $stmt->fetchAll();
         foreach ($rows as $row) {
@@ -23,36 +22,37 @@ if (isset($_SESSION['admin']) && $_SESSION['admin'] == PASS) {
         }
     }
 
-    $titleErr = $descriptionErr = $priceErr = $imageErr = "";
+    $errors = array();
     $title = $description = $price = $image = "";
     if (isset($_POST['submit'])) {
         if ($_SERVER['REQUEST_METHOD'] == "POST") {
             if (empty($_POST['title'])) {
-                $titleErr = "Title is required";
+                $errors['title'] = trans("Title is required");
             } else {
                 $title = strip_tags($_POST['title']);
                 // check if title only contains letters and whitespace
                 if (!preg_match("/^[a-zA-Z ]*$/", $title)) {
-                    $titleErr = "Only letters and white space allowed";
+                    $errors['title'] = trans("Only letters and white space allowed");
                 }
             }
 
             if (empty($_POST['description'])) {
-                $descriptionErr = "Description is required";
+                $errors['description'] = trans("Description is required");
             } else {
                 $description = strip_tags($_POST['description']);
             }
 
             if (empty($_POST['price'])) {
-                $priceErr = "Price is required";
+                $errors['price'] = trans("Price is required");
             } else {
                 $price = strip_tags($_POST['price']);
                 // check if price contains double values
                 if (!preg_match("/^[0-9]*\.[0-9]+$/", $price)) {
-                    $priceErr = "Only numbers(double) value allowed";
+                    $errors['price'] = trans("Only numbers(double) value allowed");
                 }
             }
         }
+
         if (isset($_POST['image'])) {
             $image = pathinfo($_FILES["browse"]["name"]);
             $_POST['image'] = $image['basename'];
@@ -64,19 +64,21 @@ if (isset($_SESSION['admin']) && $_SESSION['admin'] == PASS) {
 
             if (move_uploaded_file($_FILES['browse']['tmp_name'], $target_dir . $uniq)) {
                 if (isset($_SESSION['edit']) && !empty($_SESSION['edit'])) {
-                    $sql = "UPDATE Products SET title='" . $title . "', description='"
-                        . $description . "', price='"
-                        . $price . "', image='"
-                        . $uniq . "' WHERE id=" . $_SESSION['edit'][0];
+                    $stmt = $conn->prepare("UPDATE Products SET title=?, description=?, price=?, image=? WHERE id=?");
+                    $stmt->bindParam(1,$title,PDO::PARAM_STR);
+                    $stmt->bindParam(2,$description,PDO::PARAM_STR);
+                    $stmt->bindParam(3,$price,PDO::PARAM_STR);
+                    $stmt->bindParam(4,$uniq,PDO::PARAM_STR);
+                    $stmt->bindParam(5,$_SESSION['edit'][0],PDO::PARAM_INT);
                 } else {
-                    $sql = "INSERT INTO Products(title, description, price, image) VALUES('" . $title . "', '"
-                        . $description . "', '"
-                        . $price . "', '"
-                        . $uniq . "')";
+                    $stmt = $conn->prepare("INSERT INTO Products(title, description, price, image) VALUES(?, ?, ?, ?)");
+                    $stmt->bindParam(1,$title,PDO::PARAM_STR);
+                    $stmt->bindParam(2,$description,PDO::PARAM_STR);
+                    $stmt->bindParam(3,$price,PDO::PARAM_STR);
+                    $stmt->bindParam(4,$uniq,PDO::PARAM_STR);
                 }
-                $stmt = $conn->prepare($sql);
                 $stmt->execute();
-                session_unset();
+                unset($_SESSION['edit']);
             }
         }
     }
@@ -95,16 +97,16 @@ if (isset($_SESSION['admin']) && $_SESSION['admin'] == PASS) {
 <body>
 <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" enctype="multipart/form-data">
     <input type="text" name="title" placeholder="<?= trans('Title') ?>"
-           value="<?php echo isset($_POST['title']) ? $_POST['title'] : '' ?>"/>*<br/>
-    <span class="error"><?php echo $titleErr; ?></span><br/>
+           value="<?= isset($_POST['title']) ? $_POST['title'] : '' ?>"/>*<br/>
+    <span class="error"><?= isset($errors['title']) ? $errors['title'] : ''; ?></span><br/>
     <input type="text" name="description" placeholder="<?= trans('Description') ?>"
-           value="<?php echo isset($_POST['description']) ? $_POST['description'] : '' ?>"/>*<br/>
-    <span class="error"><?php echo $descriptionErr; ?></span><br/>
+           value="<?= isset($_POST['description']) ? $_POST['description'] : '' ?>"/>*<br/>
+    <span class="error"><?= isset($errors['description']) ? $errors['description'] : ''; ?></span><br/>
     <input type="text" name="price" placeholder="<?= trans('Price') ?>"
-           value="<?php echo isset($_POST['price']) ? $_POST['price'] : '' ?>"/>*<br/>
-    <span class="error"><?php echo $priceErr; ?></span><br/>
+           value="<?= isset($_POST['price']) ? $_POST['price'] : '' ?>"/>*<br/>
+    <span class="error"><?= isset($errors['price']) ? $errors['price'] : ''; ?></span><br/>
     <input type="text" name="image" placeholder="<?= trans('Image') ?>"
-           value="<?php echo isset($_POST['image']) ? $_POST['image'] : '' ?>"/>
+           value="<?= isset($_POST['image']) ? $_POST['image'] : '' ?>"/>
     <input type="file" name="browse" id="browse" value="<?= trans('Browse') ?>"><br/><br/>
     <a href="products.php"><?= trans('Products') ?></a>
     <input type="submit" name="submit" value="<?= trans('Save') ?>">
