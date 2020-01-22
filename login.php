4fceae7d19351be7be6e7462b6936e9216cb7f56
sdbisTeam <balineanu@gmail.com>
2020-01-22 18:16:57 +0200

    #6: login.php http://pm.5psolutions.net/public/index.php?path_info=projects/training-claudia-balineanu/tasks/6

--- login.php
+++ login.php
@@ -4,7 +4,7 @@ require_once 'common.php';
 setcookie(session_name(), session_id(), time() + (30 * 60));
 
 if (isset($_SESSION['admin'])) {
-    session_unset();
+    unset($_SESSION['admin']);
 } else {
     $_SESSION['admin'] = array();
 }
@@ -32,13 +32,8 @@ if (isset($_POST['submit'])) {
     if (empty($errors)) {
         if (ADMIN == $username && PASS == $password) {
             // set session admin with the password
-            $_SESSION['admin'] = $password;
-
+            $_SESSION['admin'] = PASS;
             redirect('products.php');
-        }
-    } else {
-        foreach ($errors as $error) {
-            echo $error;
         }
     }
 }
@@ -46,9 +41,14 @@ if (isset($_POST['submit'])) {
 <html>
 <head></head>
 <body>
-<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
+<?php if (isset($errors)): ?>
+    <?php foreach ($errors as $error): ?>
+        <?= $error; ?>
+    <?php endforeach; ?>
+<?php endif; ?>
+<form method="post">
     <input type="text" name="username" placeholder="<?= trans('Username') ?>"
-           value="<?php echo isset($_POST['username']) ? $_POST['username'] : '' ?>"/><br/><br/>
+           value="<?= isset($_POST['username']) ? $_POST['username'] : '' ?>"/><br/><br/>
     <input type="password" name="password" placeholder="<?= trans('Password') ?>"/><br/><br/>
     <input type="submit" name="submit" value="<?= trans('Login') ?>"/>
 </form>
