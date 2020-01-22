dc603afb5635a6fa28e19c866322317cedef3862
sdbisTeam <balineanu@gmail.com>
2020-01-22 13:45:33 +0200

    #8: product.php http://pm.5psolutions.net/public/index.php?path_info=projects/training-claudia-balineanu/tasks/8

--- product.php
+++ product.php
@@ -1,97 +1,87 @@
 <?php
 require_once 'common.php';
 
-session_start();
+setcookie('admin', session_id(), time() + (30 * 60));
 
 if (isset($_SESSION['admin']) && $_SESSION['admin'] == PASS) {
-    $now = time();
-    if ($now > $_SESSION['expire']) {
-        session_destroy();
-        // session has expired! redirect to login
-        header("Location: login.php");
-        exit();
-    } else {
 
-        if (!isset($_SESSION['edit'])) {
-            $_SESSION['edit'] = array();
+    if (!isset($_SESSION['edit'])) {
+        $_SESSION['edit'] = array();
+    }
+
+    if (isset($_GET['id'])) {
+        $_SESSION['edit'][] = $_GET['id'];
+        $sql = "SELECT * FROM Products WHERE id=" . $_GET['id'];
+        $stmt = $conn->prepare($sql);
+        $stmt->execute();
+        $rows = $stmt->fetchAll();
+        foreach ($rows as $row) {
+            $_POST['title'] = $row['title'];
+            $_POST['description'] = $row['description'];
+            $_POST['price'] = $row['price'];
+            $_POST['image'] = $row['image'];
         }
+    }
 
-        if (isset($_GET['id'])) {
-            $_SESSION['edit'][] = $_GET['id'];
-            $sql = "SELECT * FROM Products WHERE id=" . $_GET['id'];
-            $stmt = $conn->prepare($sql);
-            $stmt->execute();
-            $rows = $stmt->fetchAll();
-            foreach ($rows as $row) {
-                $_POST['title'] = $row['title'];
-                $_POST['description'] = $row['description'];
-                $_POST['price'] = $row['price'];
-                $_POST['image'] = $row['image'];
-            }
-        }
-
-        $titleErr = $descriptionErr = $priceErr = $imageErr = "";
-        $title = $description = $price = $image = "";
-        if (isset($_POST['submit'])) {
-            if ($_SERVER['REQUEST_METHOD'] == "POST") {
-                if (empty($_POST['title'])) {
-                    $titleErr = "Title is required";
-                } else {
-                    $title = strip_tags($_POST['title']);
-                    // check if title only contains letters and whitespace
-                    if (!preg_match("/^[a-zA-Z ]*$/", $title)) {
-                        $titleErr = "Only letters and white space allowed";
-                    }
-                }
-
-                if (empty($_POST['description'])) {
-                    $descriptionErr = "Description is required";
-                } else {
-                    $description = strip_tags($_POST['description']);
-                }
-
-                if (empty($_POST['price'])) {
-                    $priceErr = "Price is required";
-                } else {
-                    $price = strip_tags($_POST['price']);
-                    // check if price contains double values
-                    if (!preg_match("/^[0-9]*\.[0-9]+$/", $price)) {
-                        $priceErr = "Only numbers(double) value allowed";
-                    }
+    $titleErr = $descriptionErr = $priceErr = $imageErr = "";
+    $title = $description = $price = $image = "";
+    if (isset($_POST['submit'])) {
+        if ($_SERVER['REQUEST_METHOD'] == "POST") {
+            if (empty($_POST['title'])) {
+                $titleErr = "Title is required";
+            } else {
+                $title = strip_tags($_POST['title']);
+                // check if title only contains letters and whitespace
+                if (!preg_match("/^[a-zA-Z ]*$/", $title)) {
+                    $titleErr = "Only letters and white space allowed";
                 }
             }
-            if (isset($_POST['image'])) {
 
-                $image = pathinfo($_FILES["browse"]["name"]);
-                $_POST['image'] = $image['basename'];
+            if (empty($_POST['description'])) {
+                $descriptionErr = "Description is required";
+            } else {
+                $description = strip_tags($_POST['description']);
+            }
 
-                $target_dir = "images/";
-                $target_file = $image['filename'];
-                $imageFileType = strtolower(pathinfo($_FILES["browse"]["name"], PATHINFO_EXTENSION));
-                $uniq = uniqid() . '.' . $imageFileType;
-
-                if (move_uploaded_file($_FILES['browse']['tmp_name'], $target_dir . $uniq)) {
-                    if (isset($_SESSION['edit']) && !empty($_SESSION['edit'])) {
-                        $sql = "UPDATE Products SET title='" . $title . "', description='"
-                            . $description . "', price='"
-                            . $price . "', image='"
-                            . $uniq . "' WHERE id=" . $_SESSION['edit'][0];
-                    } else {
-                        $sql = "INSERT INTO Products(title, description, price, image) VALUES('" . $title . "', '"
-                            . $description . "', '"
-                            . $price . "', '"
-                            . $uniq . "')";
-                    }
-                    $stmt = $conn->prepare($sql);
-                    $stmt->execute();
-                    session_unset();
+            if (empty($_POST['price'])) {
+                $priceErr = "Price is required";
+            } else {
+                $price = strip_tags($_POST['price']);
+                // check if price contains double values
+                if (!preg_match("/^[0-9]*\.[0-9]+$/", $price)) {
+                    $priceErr = "Only numbers(double) value allowed";
                 }
+            }
+        }
+        if (isset($_POST['image'])) {
+            $image = pathinfo($_FILES["browse"]["name"]);
+            $_POST['image'] = $image['basename'];
+
+            $target_dir = "images/";
+            $target_file = $image['filename'];
+            $imageFileType = strtolower(pathinfo($_FILES["browse"]["name"], PATHINFO_EXTENSION));
+            $uniq = uniqid() . '.' . $imageFileType;
+
+            if (move_uploaded_file($_FILES['browse']['tmp_name'], $target_dir . $uniq)) {
+                if (isset($_SESSION['edit']) && !empty($_SESSION['edit'])) {
+                    $sql = "UPDATE Products SET title='" . $title . "', description='"
+                        . $description . "', price='"
+                        . $price . "', image='"
+                        . $uniq . "' WHERE id=" . $_SESSION['edit'][0];
+                } else {
+                    $sql = "INSERT INTO Products(title, description, price, image) VALUES('" . $title . "', '"
+                        . $description . "', '"
+                        . $price . "', '"
+                        . $uniq . "')";
+                }
+                $stmt = $conn->prepare($sql);
+                $stmt->execute();
+                session_unset();
             }
         }
     }
 } else {
-    header("Location: login.php");
-    exit();
+    redirect('login.php');
 }
 ?>
 <html>
