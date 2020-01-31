683a9a765323ded50b8958512973a957cae66fc0
sdbisTeam <balineanu@gmail.com>
2020-01-31 14:38:02 +0200

    #8: product.php http://pm.5psolutions.net/public/index.php?path_info=projects/training-claudia-balineanu/tasks/8

--- product.php
+++ product.php
@@ -36,21 +36,26 @@ if (isset($_POST['submit'])) {
             }
         }
 
-        if (empty($_FILES['browse']['name'])) {
-            $errors['image'] = trans("Select image!");
+        if (empty($_FILES['image']['name'])) {
+            $errors['image'] = trans("Select image");
         } else {
-            $_POST['image'] = $_FILES['browse']['name'];
-            $uniq = uniqid() . '.' . pathinfo($_FILES['browse']['name'], PATHINFO_EXTENSION);
             if (empty($errors)) {
-                // temporary file name on server
-                if (move_uploaded_file($_FILES['browse']['tmp_name'], TARGET_DIR . $uniq)) {
-                    if (isset($_GET['id'])) {
-                        $stmt = $conn->prepare("UPDATE products SET title = ?, description = ?, price = ?, image = ? WHERE id = ?");
-                        $stmt->execute([$title, $description, $price, $uniq, $_GET['id']]);
+                // check if the file is an image
+                if (exif_imagetype($_FILES['image']['tmp_name']) !== false) {
+                    $uniq = uniqid() . '.' . pathinfo($_FILES['image']['name'], PATHINFO_EXTENSION);
+                    if (move_uploaded_file($_FILES['image']['tmp_name'], TARGET_DIR . $uniq)) {
+                        if (isset($_GET['id'])) {
+                            $stmt = $conn->prepare("UPDATE products SET title = ?, description = ?, price = ?, image = ? WHERE id = ?");
+                            $stmt->execute([$title, $description, $price, $uniq, $_GET['id']]);
+                        } else {
+                            $stmt = $conn->prepare("INSERT INTO products(title, description, price, image) VALUES(?, ?, ?, ?)");
+                            $stmt->execute([$title, $description, $price, $uniq]);
+                        }
                     } else {
-                        $stmt = $conn->prepare("INSERT INTO products(title, description, price, image) VALUES(?, ?, ?, ?)");
-                        $stmt->execute([$title, $description, $price, $uniq]);
+                        $errors['image'] = trans("Sorry, your image was not uploaded");
                     }
+                } else {
+                    $errors['image'] = trans("File is not an image");
                 }
             }
         }
@@ -60,13 +65,10 @@ if (isset($_POST['submit'])) {
 if (isset($_GET['id'])) {
     $stmt = $conn->prepare("SELECT * FROM products WHERE id = ?");
     $stmt->execute([$_GET['id']]);
-    $rows = $stmt->fetchAll();
-    foreach ($rows as $row) {
-        $_POST['title'] = $row['title'];
-        $_POST['description'] = $row['description'];
-        $_POST['price'] = $row['price'];
-        $_POST['image'] = $row['image'];
-    }
+    $row = $stmt->fetch(PDO::FETCH_ASSOC);
+    $title = $row['title'];
+    $description = $row['description'];
+    $price = $row['price'];
 }
 ?>
 <html>
@@ -80,17 +82,15 @@ if (isset($_GET['id'])) {
 <body>
 <form method="post"  enctype="multipart/form-data">
     <input type="text" name="title" placeholder="<?= trans('Title') ?>"
-           value="<?= isset($_POST['title']) ? $_POST['title'] : '' ?>"/>*<br/>
+           value="<?= isset($_POST['title']) ? $_POST['title'] : $title ?>"/>*<br/>
     <span class="error"><?= isset($errors['title']) ? $errors['title'] : ''; ?></span><br/>
     <input type="text" name="description" placeholder="<?= trans('Description') ?>"
-           value="<?= isset($_POST['description']) ? $_POST['description'] : '' ?>"/>*<br/>
+           value="<?= isset($_POST['description']) ? $_POST['description'] : $description ?>"/>*<br/>
     <span class="error"><?= isset($errors['description']) ? $errors['description'] : ''; ?></span><br/>
     <input type="text" name="price" placeholder="<?= trans('Price') ?>"
-           value="<?= isset($_POST['price']) ? $_POST['price'] : '' ?>"/>*<br/>
+           value="<?= isset($_POST['price']) ? $_POST['price'] : $price ?>"/>*<br/>
     <span class="error"><?= isset($errors['price']) ? $errors['price'] : ''; ?></span><br/>
-    <input type="text" name="image" placeholder="<?= trans('Image') ?>"
-           value="<?= isset($_POST['image']) ? $_POST['image'] : '' ?>"/>
-    <input type="file" name="browse" id="browse" value="<?= trans('Browse') ?>"><br/><br/>
+    <input type="file" name="image" id="image" value="<?= trans('Browse') ?>"><br/><br/>
     <span class="error"><?= isset($errors['image']) ? $errors['image'] : ''; ?></span><br/>
     <a href="products.php"><?= trans('Products') ?></a>
     <input type="submit" name="submit" value="<?= trans('Save') ?>">
